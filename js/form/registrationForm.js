e6eac28b5c9bde45d46c78b058d096a15df8cabd
Krishna18-krish <krishnamoorthy.duraisamy@fssa.freshworks.com>
2024-11-08 21:18:32 +0530

    style and path is and color change

--- js/form/registrationForm.js
+++ js/form/registrationForm.js
@@ -77,7 +77,7 @@ registrationForm.addEventListener("submit", (event) => {
         // Registration successful
         const user = userCredential.user;
         console.log("User:", user);
-        window.location.href="../../html/homepage.html"
+        window.location.href="../../homepage.html";
       })
       .catch((error) => {
         // Handle registration errors
